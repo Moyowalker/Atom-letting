13cdae944e88724c3354bc085f3d0689a7a2c2fd
Moyosore Ajayi <ajayimy@gmail.com>
2022-06-24 15:00:37 +0100

    testing search functionality

---
+++ app.js
@@ -0,0 +1,20 @@
+const search = () => {
+    const searchbox = document.getElementById("search-property").value.toUpperCase();
+    const storeitems = document.getElementById("property-list");
+    const properties = document.querySelectorAll(".properties");
+    const pname = storeitems.getElementsByTagName("h3");
+
+    for(var i=0; i < pname.length; i++){
+        let match = properties[i].getElementsByTagName("h3")[0];
+
+        if(match){
+            let textvalue = match.textContent || match.innerHTML
+
+            if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+                properties[i].style.display = "";
+            } else {
+                properties[i].style.display = "none";
+            }
+        }
+    }
+}
